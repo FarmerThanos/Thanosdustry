379b260a3bedc5e5dc0646304836bf00bc5409ab
Farmer Thanos <wiktorlk@hotmail.com>
2021-01-30 15:59:39 +0100

    Changed stuff

--- src/farmerthanos/thanosdustry/Thanosdustry.java
+++ src/farmerthanos/thanosdustry/Thanosdustry.java
@@ -1,8 +1,8 @@
-package sonnicon.modtemplate;
+package farmerthanos.thanosdustry;
 
 import mindustry.mod.Mod;
 
-public class ModTemplate extends Mod{
+public class Thanosdustry extends Mod{
 
     @Override
     public void init(){
