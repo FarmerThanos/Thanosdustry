af8657bf55ec3131920f316c1cb0d325973e29dc
Farmer Thanos <wiktorlk@hotmail.com>
2021-02-04 14:51:55 +0100

    Modified code

--- src/farmerthanos/thanosdustry/Thanosdustry.java
+++ src/farmerthanos/thanosdustry/Thanosdustry.java
@@ -1,12 +1,22 @@
 package farmerthanos.thanosdustry;
 
+import arc.Events;
 import arc.util.Log;
 import farmerthanos.thanosdustry.content.ThBlocks;
 import farmerthanos.thanosdustry.content.ThItems;
+import farmerthanos.thanosdustry.content.ThSounds;
 import farmerthanos.thanosdustry.content.ThUnits;
+import mindustry.game.EventType;
 import mindustry.mod.Mod;
 
 public class Thanosdustry extends Mod{
+    public Thanosdustry() {
+        ThSounds.load();
+
+        Events.on(EventType.DisposeEvent.class, e -> {
+            ThSounds.dispose();
+        });
+    }
 
     @Override
     public void init(){
